79a8371afcd103f3a4a713daa92ebfe5119f461e
erlandoss <32767083+erlandoss@users.noreply.github.com>
2019-11-02 18:37:06 +0100

    test 2

---
+++ push.sh
@@ -0,0 +1,5 @@
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker build . --tag applikasjon --build-arg JAR_FILE=./build/libs/applikasjon-0.0.1-SNAPSHOT.jar
+docker run -p 8080:8080 applikasjon:latest
+docker tag applikasjon $DOCKER_USERNAME/eksamendevops
+docker push $DOCKER_USERNAME/eksamendevops
